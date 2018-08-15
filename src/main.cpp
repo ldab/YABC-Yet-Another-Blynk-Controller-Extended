a240a44abeb190c1962936bba9623ecdebce0c37
ldab <34199302+ldab@users.noreply.github.com>
2018-08-16 09:47:45 +1200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-
-/*
-  YABC-Yet-Another-Blynk-Controller-Extended
+/*  YABC-Yet-Another-Blynk-Controller-Extended
   Copyright (c) 2018 Leonardo Bispo.  All right reserved.
   This code is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -12,8 +10,7 @@
   Lesser General Public License for more details.
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
 
 #include <FS.h>                   //this needs to be first, or it all crashes and burns...
 
